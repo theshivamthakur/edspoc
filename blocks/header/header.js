516d5d1e8ff3a94bdc4a458aed8db4c164ce257f
Shivam Thakur <shivam.thakur@axeno.co>
2025-10-24 15:58:33 +0530

    testing

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -1,12 +1,14 @@
 export default function decorate(block) {
-  console.log('Total rows:', block.children.length);
+  console.log('Total rows:', block.children.children.length);
   
   [...block.children].forEach((row, i) => {
     console.log(`Row ${i}:`, row.outerHTML);
-    console.log(`Row ${i} has ${row.children.length} cells`);
+    console.log(`Row ${i} has ${row.children.children.length} cells`);
     
-    [...row.children].forEach((cell, j) => {
+    [...row.children].forEach((child) => {
+      [...child.children].forEach((cell, j) => {
       console.log(`  Cell ${j}:`, cell.innerHTML);
+    })
     });
   });
   
