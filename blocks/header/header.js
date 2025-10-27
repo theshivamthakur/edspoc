988d96f9146f0369b636ad2e03cc007ec9f9cb4a
Shivam Thakur <shivam.thakur@axeno.co>
2025-10-27 12:33:40 +0530

    testing

---
+++ blocks/header/header.js
@@ -0,0 +1,11 @@
+import { readBlockConfig } from '../../scripts/aem.js';
+
+/**
+ * Renders the header block with navigation, logo, and login
+ * @param {Element} block The header block element
+ */
+export default async function decorate(block) {
+console.log(block);
+
+
+}
