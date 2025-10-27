651de3d3fdf3f32f265350bfd141f6328ad23e0a
Shivam Thakur <shivam.thakur@axeno.co>
2025-10-27 15:20:24 +0530

    test

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -1,4 +1,8 @@
 export default function decorate(block) {
  console.log("This is block",block)
  console.log([...block.children])
+  const model = window.XWalk?.getBlockModelData
+    ? window.XWalk.getBlockModelData(block)
+    : {}; // Or fetch inline JSON or data- props
+console.log(model)
 }
