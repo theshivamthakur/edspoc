b6443f24a7625207cced2379e696566fbf882d08
Shivam Thakur <shivam.thakur@axeno.co>
2025-10-27 15:40:44 +0530

    test

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -2,12 +2,5 @@ export default function decorate(block) {
 const rawModel = block.getAttribute('data-aue-model');
 let modelData = null;
 
-if (rawModel) {
-  try {
-    modelData = JSON.parse(rawModel);
-    console.log('Parsed block model:', modelData);
-  } catch (e) {
-    console.error('Failed to parse block model', e, rawModel);
-  }
-}
+console.log(rawModel);
 }
