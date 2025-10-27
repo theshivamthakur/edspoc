fe40c4d9d45659fb2ef84432334d4b2c312d7b60
Shivam Thakur <shivam.thakur@axeno.co>
2025-10-27 14:47:21 +0530

    testing

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -1,25 +1,3 @@
-export default function decorate(b) {
-  b.classList.add('header-block');
-  const appName = b.querySelector('.header-app-name');
-  if (appName) appName.classList.add('header-block-app-name');
-  const navMenuItems = b.querySelectorAll('.header-sidebar-menu-item');
-  navMenuItems.forEach(li => {
-    li.classList.add('header-block-nav-menu-item');
-    const a = li.querySelector('a');
-    if (a) a.classList.add('header-block-nav-menu-link');
-    const img = a ? a.querySelector('img') : null;
-    if (img) img.classList.add('header-block-nav-menu-icon');
-  });
-  const footerLinks = b.querySelectorAll('.header-footer-list-item a');
-  footerLinks.forEach(a => {
-    a.classList.add('header-block-footer-link');
-  });
-  const footerSocialLinks = b.querySelectorAll('.header-footer-brand-right--list .header-footer-brand-right--link');
-  footerSocialLinks.forEach(a => {
-    a.classList.add('header-block-footer-social-link');
-    const img = a.querySelector('img');
-    if (img) img.classList.add('header-block-footer-social-icon');
-  });
-  const copyright = b.querySelector('.header-footer-brand-left--copyright');
-  if (copyright) copyright.classList.add('header-block-footer-copyright');
+export default function decorate(block) {
+ console.log("This is block",block)
 }
