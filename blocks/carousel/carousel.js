4ec7ca63edca9112410e0cd8460e9da8c71f74ec
Shivam Thakur <shivam.thakur@axeno.co>
2025-11-04 14:10:47 +0530

    test

--- blocks/carousel/carousel.js
+++ blocks/carousel/carousel.js
@@ -1,4 +1,5 @@
 import { createOptimizedPicture } from '../../scripts/aem.js';
+import { moveInstrumentation } from '../../scripts/scripts.js';
 
 export default function decorate(block) {
   const carouselPositionRelative = block.querySelector('.carousel-position-relative');
@@ -8,83 +9,69 @@ export default function decorate(block) {
 
   const swiperWrapper = block.querySelector('.swiper-wrapper');
   if (swiperWrapper) {
-    swiperWrapper.classList.add('carousel-slides');
+    swiperWrapper.classList.add('carousel-container');
   }
 
-  const slides = block.querySelectorAll('.swiper-slide');
-  slides.forEach((slide) => {
+  const swiperSlides = block.querySelectorAll('.swiper-slide');
+  swiperSlides.forEach((slide) => {
     slide.classList.add('carousel-slide');
-    const bannerDiv = slide.querySelector('.carousel-banner');
-    if (bannerDiv) {
-      bannerDiv.classList.add('carousel-slide-content');
-    }
 
     const videoWrapper = slide.querySelector('.carousel-video-wrapper');
     if (videoWrapper) {
+      videoWrapper.classList.add('carousel-media-wrapper');
       const video = videoWrapper.querySelector('video');
       if (video) {
-        video.classList.add('carousel-media', 'carousel-video');
-        const sources = video.querySelectorAll('source');
-        sources.forEach(source => {
-          if (source.src) {
-            source.src = source.src;
-          }
-        });
-        videoWrapper.innerHTML = '';
-        videoWrapper.append(video);
+        video.classList.add('carousel-video');
+        video.removeAttribute('data-is-autoplay');
+        video.removeAttribute('data-autopause-disabled');
+        video.removeAttribute('loop');
+        video.removeAttribute('muted');
+        video.removeAttribute('autoplay');
       }
+
       const playButton = videoWrapper.querySelector('.carousel-icon-play');
-      const pauseButton = videoWrapper.querySelector('.carousel-icon-pause');
-      const muteButton = videoWrapper.querySelector('.carousel-icon-mute');
-      const unmuteButton = videoWrapper.querySelector('.carousel-icon-unmute');
-      const noAudioButton = videoWrapper.querySelector('.carousel-no-audio-icon');
-
-      if (playButton) playButton.remove();
-      if (pauseButton) pauseButton.remove();
-      if (muteButton) muteButton.remove();
-      if (unmuteButton) unmuteButton.remove();
-      if (noAudioButton) noAudioButton.remove();
-
-    } else {
-      const img = slide.querySelector('img');
-      if (img) {
-        img.classList.add('carousel-media', 'carousel-image');
-        const optimizedPic = createOptimizedPicture(img.src, img.alt, false, [{ width: '1200' }]);
-        img.closest('picture')?.replaceWith(optimizedPic);
+      if (playButton) {
+        playButton.remove();
       }
+      const pauseButton = videoWrapper.querySelector('.carousel-icon-pause');
+      if (pauseButton) {
+        pauseButton.remove();
+      }
+      const muteIconContainer = videoWrapper.querySelector('.carousel-mute-icon');
+      if (muteIconContainer) {
+        muteIconContainer.remove();
+      }
+    }
+
+    const img = slide.querySelector('img');
+    if (img) {
+      img.classList.add('carousel-image');
+      const optimizedPic = createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }]);
+      moveInstrumentation(img, optimizedPic.querySelector('img'));
+      img.closest('img').replaceWith(optimizedPic);
     }
 
     const ctaWrapper = slide.querySelector('.carousel-banner-cta');
     if (ctaWrapper) {
-      const ctaLink = ctaWrapper.querySelector('a.carousel-cmp-button');
+      ctaWrapper.classList.add('carousel-cta-wrapper');
+      const ctaLink = ctaWrapper.querySelector('a');
       if (ctaLink) {
         ctaLink.classList.add('carousel-cta-link');
-        const span = ctaLink.querySelector('span');
-        if (span) {
-          span.classList.add('carousel-cta-text');
+        const ctaTextSpan = ctaLink.querySelector('.carousel-cmp-button__text');
+        if (ctaTextSpan) {
+          ctaTextSpan.classList.add('carousel-cta-text');
         }
-        ctaWrapper.innerHTML = '';
-        ctaWrapper.append(ctaLink);
+      }
+      const popup = ctaWrapper.querySelector('.carousel-pop-up');
+      if (popup) {
+        popup.remove();
+      }
+      const externalLinkPopup = ctaWrapper.querySelector('.carousel-external-link-popup');
+      if (externalLinkPopup) {
+        externalLinkPopup.remove();
       }
     }
   });
-
-  const pagination = block.querySelector('.carousel-swiper-pagination');
-  if (pagination) {
-    pagination.classList.add('carousel-pagination');
-  }
-
-  const prevButton = block.querySelector('.carousel-primary-swiper__buttonPrev');
-  if (prevButton) {
-    prevButton.classList.add('carousel-prev-button');
-    prevButton.innerHTML = '<span class="carousel-icon"></span>';
-  }
-
-  const nextButton = block.querySelector('.carousel-primary-swiper__buttonNext');
-  if (nextButton) {
-    nextButton.classList.add('carousel-next-button');
-    nextButton.innerHTML = '<span class="carousel-icon"></span>';
-  }
 
   const actions = block.querySelector('.carousel-cmp-carousel__actions');
   if (actions) {
@@ -93,48 +80,71 @@ export default function decorate(block) {
 
   const swiperContainer = block.querySelector('.carousel-swiper-container');
   if (swiperContainer) {
-    swiperContainer.classList.add('carousel-navigation');
+    swiperContainer.remove();
   }
 
-  const swiperElement = block.querySelector('.swiper');
-  if (swiperElement) {
-    swiperElement.classList.add('carousel-container');
+  const pagination = block.querySelector('.carousel-swiper-pagination');
+  if (pagination) {
+    pagination.remove();
   }
 
-  // Remove popup elements
-  const popUp = block.querySelector('.carousel-pop-up');
-  if (popUp) popUp.remove();
-  const externalLinkPopup = block.querySelector('.carousel-external-link-popup');
-  if (externalLinkPopup) externalLinkPopup.remove();
+  // Remove the data-swiper-id, data-is-autoplay, data-delay, data-autopause-disabled, data-is-loop, data-placeholder-text attributes from the main swiper div
+  const mainSwiperDiv = block.querySelector('.swiper.carousel-primary-swiper');
+  if (mainSwiperDiv) {
+    mainSwiperDiv.removeAttribute('data-swiper-id');
+    mainSwiperDiv.removeAttribute('data-is-autoplay');
+    mainSwiperDiv.removeAttribute('data-delay');
+    mainSwiperDiv.removeAttribute('data-autopause-disabled');
+    mainSwiperDiv.removeAttribute('data-is-loop');
+    mainSwiperDiv.removeAttribute('data-placeholder-text');
+    mainSwiperDiv.removeAttribute('id');
+    mainSwiperDiv.removeAttribute('role');
+    mainSwiperDiv.removeAttribute('aria-live');
+    mainSwiperDiv.removeAttribute('aria-roledescription');
+  }
 
-  // Clean up remaining elements if they don't fit the model
-  const elementsToRemove = [
-    '.carousel-banner-section',
-    '.carousel-position-absolute.carousel-w-100.carousel-h-100',
-    '.carousel-position-absolute.carousel-z-2',
-    '.carousel-position-absolute.carousel-start-50'
-  ];
-
-  elementsToRemove.forEach(selector => {
-    const el = block.querySelector(selector);
-    if (el && !el.classList.contains('carousel-slide-content')) {
-      el.remove();
+  const carouselBannerDivs = block.querySelectorAll('.carousel-banner');
+  carouselBannerDivs.forEach((div) => {
+    const section = div.querySelector('.carousel-banner-section');
+    if (section) {
+      while (section.firstElementChild) {
+        div.append(section.firstElementChild);
+      }
+      section.remove();
     }
   });
 
-  // Ensure the main block only contains the necessary structure
-  const finalContent = document.createElement('div');
-  finalContent.classList.add('carousel-content');
-  if (swiperElement) {
-    finalContent.append(swiperElement);
-  }
-  if (swiperContainer) {
-    finalContent.append(swiperContainer);
-  }
-  if (pagination) {
-    finalContent.append(pagination);
+  const bannerSectionWrappers = block.querySelectorAll('.carousel-banner-section__wrapper');
+  bannerSectionWrappers.forEach((wrapper) => {
+    wrapper.classList.add('carousel-item-content');
+    const ctaDiv = wrapper.querySelector('.carousel-boing__banner--cta');
+    if (ctaDiv) {
+      while (ctaDiv.firstElementChild) {
+        wrapper.append(ctaDiv.firstElementChild);
+      }
+      ctaDiv.remove();
+    }
+  });
+
+  const allSwiperSlides = block.querySelectorAll('.swiper-slide');
+  allSwiperSlides.forEach((slide) => {
+    slide.removeAttribute('data-active');
+    slide.removeAttribute('id');
+    slide.removeAttribute('role');
+    slide.removeAttribute('aria-labelledby');
+    slide.removeAttribute('aria-roledescription');
+    slide.removeAttribute('data-cmp-data-layer');
+    slide.removeAttribute('data-cmp-hook-carousel');
+    slide.removeAttribute('style');
+  });
+
+  const swiperWrapperDiv = block.querySelector('.swiper-wrapper');
+  if (swiperWrapperDiv) {
+    swiperWrapperDiv.removeAttribute('style');
   }
 
-  block.innerHTML = '';
-  block.append(finalContent);
+  // Remove all the classes that start with 'swiper-'
+  block.querySelectorAll('[class*="swiper-"]').forEach((el) => {
+    el.className = Array.from(el.classList).filter((cls) => !cls.startsWith('swiper-')).join(' ');
+  });
 }
